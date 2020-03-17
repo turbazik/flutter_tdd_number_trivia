c708087351c733c2447a4e7db2f03e7214a6fc05
turbazik <gaziztursynbek@gmail.com>
2020-03-17 15:44:12 +0600

    add repositories and usecases in domain also testing

--- lib/features/number_trivia/domain/entities/number_trivia.dart
+++ lib/features/number_trivia/domain/entities/number_trivia.dart
@@ -1,10 +1,9 @@
 import 'package:equatable/equatable.dart';
-import 'package:flutter/cupertino.dart';
 
 class NumberTrivia extends Equatable {
   final String text;
   final int number;
 
-  NumberTrivia(@required this.text, @required this.number)
+  NumberTrivia(this.text, this.number)
       : super([text, number]);
 }
